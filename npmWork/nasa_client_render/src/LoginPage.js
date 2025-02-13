f32bb3387f3d8cfaf990c453ce0db46714bf4095
Manisha Ganguly(Nandi) <nandi.manisha@gmail.com>
2025-02-13 12:14:36 +0000

    Server Changed with MongoDB and thus clients were tested in locally

--- npmWork/nasa_client_render/src/LoginPage.js
+++ npmWork/nasa_client_render/src/LoginPage.js
@@ -10,7 +10,7 @@ const response = await axios.post("https://nasa-photo-demo.onrender.com/api/logi
 localStorage.setItem("user", response.data.username); 
 setUser(response.data.username); navigate("/gallery"); } catch (err) { setError("Invalid username or password"); } }; 
 return ( <div style={{ textAlign: "center", marginTop: "50px" }}> 
-<h2>Login</h2> <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} style={{ padding: "8px", margin: "5px" }} /> 
+<h2>Login to view Nasa Photo Gallery</h2> <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} style={{ padding: "8px", margin: "5px" }} />
 <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ padding: "8px", margin: "5px" }} /> 
 <button onClick={handleLogin} style={{ padding: "8px 20px", marginTop: "10px" }}> Login </button> {error && <p style={{ color: "red" }}>{error}</p>} </div> ); } 
 export default LoginPage;
