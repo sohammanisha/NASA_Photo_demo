bcdf386a8e5bc06b3f5fcc5544e72caa23f95297
Manisha Ganguly(Nandi) <nandi.manisha@gmail.com>
2025-02-14 11:50:37 +0000

    Models and Searched modified to work

--- npmWork/nasa_client_render/src/LoginPage.js
+++ npmWork/nasa_client_render/src/LoginPage.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react"; import axios from "axios"; import { useNavigate } from "react-router-dom";
 function LoginPage({ setUser }) { const [username, setUsername] = useState(""); const [password, setPassword] = useState("");
     const [error, setError] = useState(""); const navigate = useNavigate(); const handleLogin = async () => { try
-    { const response = await axios.post("https://nasa-photo-demo.onrender.com//api/login", { username, password }); localStorage.setItem("user", response.data.username);
-        setUser(response.data.username); navigate("/gallery"); } catch (err) { setError("Invalid username or password"); } };
+    { const response =
+        await axios.post("https://nasa-photo-demo.onrender.com/api/login", { username, password });
+        localStorage.setItem("user", response.data.username);
+        setUser(response.data.username); navigate("/gallery"); }
+    catch (err) { setError("Invalid username or password"); } };
     return ( <div style={{ textAlign: "center", marginTop: "50px" }}>
         <h2>Login</h2> <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} style={{ padding: "8px", margin: "5px" }} />
         <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ padding: "8px", margin: "5px" }} />
